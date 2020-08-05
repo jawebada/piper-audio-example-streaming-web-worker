74761628d6afe98a165b1b3288fa746016411b0f
Jan Weil <jan.weil@web.de>
2020-08-05 11:15:10 +0200

    Add basic webpack skeleton

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+    worker: './src/worker.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
