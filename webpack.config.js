55e9540b23ebe51625ab16443e38a512e46f5d94
Jan Weil <jan.weil@web.de>
2020-08-07 11:50:07 +0200

    Enable dev mode, add watch script

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,19 @@ const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
     worker: './src/worker.js'
   },
+  devtool: 'inline-source-map',
   plugins: [
-    new CleanWebpackPlugin()
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'docs'),
   },
   node: {
-    fs: "empty",
+    fs: 'empty',
   }
 }
